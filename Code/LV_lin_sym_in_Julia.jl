6305613b4683198993e352041b4746997e3e8ec6
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2022-05-17 11:46:20 +0100

    Write function in Julia for allocating all our 'symbolic monomials' if you will

--- Code/LV_lin_sym_in_Julia.jl
+++ Code/LV_lin_sym_in_Julia.jl
@@ -14,10 +14,59 @@
 # =================================================================================
 # =================================================================================
 #using Pkg
+#Pkg.add("TypedPolynomials")
+#using TypedPolynomials
+#Pkg.add("DynamicPolynomials")
+using DynamicPolynomials
+#Pkg.add("MultivariateBases")
+using MultivariateBases
+#Pkg.add("SymPoly")
+#using SymPoly
+#Pkg.add("SymbolicUtils")
+#using SymbolicUtils
 #Pkg.add("Symbolics")
 using Symbolics
 #Pkg.add("Latexify")
 using Latexify
+#Pkg.add("Combinatorics")
+#Pkg.add("BenchmarkTools")
+#using Combinatorics, BenchmarkTools
+#using Combinatorics
+# =================================================================================
+# =================================================================================
+# Functions
+# =================================================================================
+# =================================================================================
+# Function 1: allocate_monomials
+# This functions takes two inputs:
+# 1. An integer named degree which determines the degree of the polynomial,
+# 2. A vector of variables containing our symbolic dependent and independent variables.
+# The function returns one output:
+# 1. A vector basis_vec_states containing all the basis monomials in terms of the states.
+function allocate_monomials(degree::Int64,variables::Vector{Num})
+    # The number of variables
+    num_variables = length(variables)
+    # Define a polynomial array with our variables    
+    eval(Meta.parse(string.("@polyvar x[1:",string.(num_variables),"]")))
+    # Define a list of all our monomials in our multivariate polynomial
+    basis_vec = MultivariateBases.monomials(x, 0:degree)
+    # Extract the exponents for each monomial as a tuple
+    exponents_of_basis = [exponents(monomial) for monomial in basis_vec]
+    # Allocate memory for basis functions in terms of the states
+    basis_vec_states = []
+    # Loop over all monomials in terms of their exponents
+    for (outer_index,exponent) in enumerate(exponents_of_basis)
+        # Allocate a temporary monomial
+        temp_monomial = 1
+        # Loop over all powers in our current exponent and construct our new basis monomial
+        for (inner_index,power) in enumerate(exponent)
+            temp_monomial *= variables[inner_index]^power
+        end
+        # Append our new basis function to our list of basis functions in terms of the states
+        append!(basis_vec_states,temp_monomial)
+    end
+    return basis_vec_states
+end
 # =================================================================================
 # =================================================================================
 # The script starts
@@ -64,6 +113,7 @@ println("The tangents are:\n")
 for tangent in tangents
     println(tangent)
 end
-
+# Create the tangential ansatze
+monomials = allocate_monomials(2,variables)
 
 
